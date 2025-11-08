974a814fad4ee9e768748eb06366d10282f0dd62
AndreiKasimovskii <kasimovskiy.a.d@gmail.com>
2025-11-08 21:08:23 +0300

    CTT-9: Add general entities

---
+++ src/TaskTracker.Domain/Entities/TodoTask.cs
@@ -0,0 +1,18 @@
+﻿namespace TaskTracker.Domain.Entities;
+
+public class TodoTask
+{
+    public int Id { get; set; }
+
+    public required string Title { get; set; }
+
+    public string? Description { get; set; }
+
+    public DateTimeOffset CreateDate { get; set; }
+
+    public TodoTaskStatus Status { get; set; }
+
+    public DateTimeOffset? DeadLineDate { get; set; }
+
+    public DateTimeOffset? CompleteDate { get; set; }
+}
---
+++ src/TaskTracker.Domain/Entities/TodoTaskStatus.cs
@@ -0,0 +1,10 @@
+namespace TaskTracker.Domain.Entities;
+
+public enum TodoTaskStatus
+{
+    Active = 1,
+
+    Complete = 2,
+
+    NotActual = 3
+}
