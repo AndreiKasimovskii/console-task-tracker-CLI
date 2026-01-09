d1777f7bf37147f7dbceb8e720f3b25603c62c70
AndreiKasimovskii <kasimovskiy.a.d@gmail.com>
2026-01-09 16:30:46 +0300

    implement restore features

--- src/TaskTracker.Domain/Entities/UserTask.cs
+++ src/TaskTracker.Domain/Entities/UserTask.cs
@@ -1,8 +1,11 @@
+using System.IO.Compression;
+using Microsoft.VisualBasic;
+
 namespace TaskTracker.Domain.Entities;
 
 public class UserTask(string title)
 {
-    public long Id { get; set; }
+    public long Id { get; init; }
 
     public string Title { get; private set; } = title;
 
@@ -13,6 +16,16 @@ public class UserTask(string title)
     public UserTaskStatus Status { get; private set; } = UserTaskStatus.Active;
 
     public DateTimeOffset? Deadline { get; private set; }
+
+    private UserTask(long id, string title, DateTimeOffset createdDate, UserTaskStatus status, string? description, DateTimeOffset? deadline)
+        : this(title)
+    {
+        Id = id;
+        CreatedDate = createdDate;
+        Status = status;
+        Description = description;
+        Deadline = deadline;
+    }
 
     #region Methods
     public bool TryChangeStatus(UserTaskStatus newStatus)
@@ -45,5 +58,39 @@ public class UserTask(string title)
         Deadline = deadline;
         return true;
     }
+
+    internal static UserTask Restore(long id, string title, string? description,
+        DateTimeOffset createdDate, string? status, DateTimeOffset? deadline)
+    {
+        var validationResult = ValidateRestoredParameters(id, title, createdDate, status, deadline);
+        if(validationResult.Success)
+            return new(id, title, createdDate, validationResult.ParsedTaskStatus, description, deadline);
+        else
+            throw new StorageCorruptedException(validationResult.FailedMessage);
+    }
+
+    private static ValidationRestoredParametersResult ValidateRestoredParameters(long id, string title, DateTimeOffset createdDate, string? status, DateTimeOffset? deadline)
+    {
+        if(id < 0)
+            return new(false, "Error! Id lower then 0.");
+
+        if(string.IsNullOrWhiteSpace(title))
+            return new(false, "Error! Title is empty.");
+
+        if(createdDate.Equals(default))
+            return new(false, "Error! Creation date not set.");
+
+        if(!Enum.TryParse<UserTaskStatus>(status, out var statusValue))
+            return new(false, "Error! Task status has unknown value.");
+
+        if(deadline.HasValue && deadline.Value.CompareTo(createdDate) < 0)
+            return new(false, "Error! Task deadline cannot be earlier than the creation date");
+
+        return new(true, ParsedTaskStatus: statusValue);
+    }
+
+    private record ValidationRestoredParametersResult(bool Success, string? FailedMessage = null, UserTaskStatus ParsedTaskStatus = default);
     #endregion
 }
+
+public class StorageCorruptedException(string? message) : Exception(message) { }
