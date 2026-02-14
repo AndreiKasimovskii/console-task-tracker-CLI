480b3fcf54a9008719ccaab8e8e87c170fb5f73a
AndreiKasimovskii <kasimovskiy.a.d@gmail.com>
2026-02-14 13:24:13 +0300

    Move initialize all dependencies to root program

--- src/TaskTracker.Application/Program.cs
+++ src/TaskTracker.Application/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace TaskTracker.Application;
+﻿using TaskTracker.Domain.Abstractions;
+using TaskTracker.Domain.Services;
+using TaskTracker.Infrastructure;
+
+namespace TaskTracker.Application;
 
 internal class Program
 {
     private static async Task Main(string[] args)
     {
-        var dispatcher = new CommandDispatcher(new ConsolePresenter());
+        var store = new FileStore();
+        IUserTaskRepository repository = new UserTaskRepository(store);
+        IUserTaskService service = new UserTaskService(repository);
+        var dispatcher = new CommandDispatcher(new ConsolePresenter(), service);
         await dispatcher.Run(args);
     }
 }
