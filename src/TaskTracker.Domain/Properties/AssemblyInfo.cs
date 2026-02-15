1a9c931d3919ed740aec783f6cadd982bca50764
AndreiKasimovskii <kasimovskiy.a.d@gmail.com>
2026-02-15 15:44:40 +0300

    mark Domain.Test project as friend

--- src/TaskTracker.Domain/Properties/AssemblyInfo.cs
+++ src/TaskTracker.Domain/Properties/AssemblyInfo.cs
@@ -1,3 +1,5 @@
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("TaskTracker.Infrastructure")]
+[assembly: InternalsVisibleTo("TaskTracker.Domain.Tests")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
